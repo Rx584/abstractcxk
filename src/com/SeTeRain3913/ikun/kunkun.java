1a8705a8be4ac19985d1b17e071925f45f4b129a
Rx584 <xiao_2010@outlook.com>
2023-05-01 18:10:55 +0800

    完成了主类，能够启动抽象坤坤

---
+++ src/com/SeTeRain3913/ikun/kunkun.java
@@ -0,0 +1,113 @@
+package com.SeTeRain3913.ikun;
+import com.SeTeRain3913.Person.cxk;
+
+public class kunkun {
+    public static void main(String[] args) {
+        cxk kunkun = new cxk();
+        kunkun.name="蔡徐坤";
+        kunkun.Do("鞠躬");
+        kunkun.AboutMe();
+
+        kunkun.moveHands(true,true,false,false);
+        kunkun.throwCoat();
+
+        kunkun.getBasketball();
+        kunkun.moveHands(true,true,false,false);
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.turn();
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.turn();
+        kunkun.playBasketball();
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.playBasketball();
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.playBasketball();
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.playBasketball();
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.playBasketball();
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.playBasketball();
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.playBasketball();
+
+        kunkun.moveHands(true,true,false,false);
+        kunkun.playBasketball();
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.turn();
+        kunkun.playBasketball();
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.turn();
+        kunkun.playBasketball();
+        kunkun.throwBasketball();
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.turn();
+        kunkun.ironKun();
+        kunkun.sing(1);
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.ironKun();
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.ironKun();
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.ironKun();
+        kunkun.turn();
+        kunkun.sing(2);
+        kunkun.moveHands(true,true,true,true);
+        kunkun.jump();
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.pantsDown();
+
+        kunkun.moveHands(true,true,true,true);
+
+        kunkun.moveHands(true,true,true,true);
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.jump();
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.pantsDown();
+
+        kunkun.moveHands(true,true,true,true);
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.sing(3);
+        kunkun.moveHands(true,true,true,true);
+        kunkun.pantsDown();
+
+        kunkun.moveHands(true,true,true,true);
+
+        kunkun.moveHands(true,true,true,true);
+
+        kunkun.moveHands(true,true,true,true);
+
+        kunkun.moveHands(true,true,true,true);
+
+        kunkun.moveHands(true,true,true,true);
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.pantsDown();
+
+        kunkun.moveHands(true,true,true,true);
+        kunkun.pantsDown();
+        kunkun.sing(4);
+
+        kunkun.moveHands(true,true,false,false);
+        kunkun.sing(5);
+        kunkun.TheEnd();
+    }
+}
